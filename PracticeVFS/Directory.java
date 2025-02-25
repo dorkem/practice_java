b24ea9d88eab37bbf7b8e4716b1ccecbe8c75dd6
dorkem <chlwogur1000@gmail.com>
2025-02-25 18:00:12 +0900

    :sparkles: impletes directory

--- PracticeVFS/Directory.java
+++ PracticeVFS/Directory.java
@@ -1,4 +1,22 @@
 package java_practice.PracticeVFS;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Directory {
+    private String name;
+    private Directory parent;
+    private Map<String, Directory> directories;
+    private Map<String, FileEntry> files;
+
+    public Directory(String name, Directory parent) {
+        this.name = name;
+        this.parent = parent;
+        this.directories = new HashMap<>();
+        this.files = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
 }
