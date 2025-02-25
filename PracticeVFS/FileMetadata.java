82ba7c6a449330af13957a8b35b7ff3442b850ab
dorkem <chlwogur1000@gmail.com>
2025-02-25 22:23:15 +0900

    :sparkles: implete manage metadata

--- PracticeVFS/FileMetadata.java
+++ PracticeVFS/FileMetadata.java
@@ -1,4 +1,22 @@
 package java_practice.PracticeVFS;
 
 public class FileMetadata {
+    private long size;
+    private long usedSize;
+    public FileMetadata(long sizeByte) {
+        size = sizeByte;
+        usedSize = 0;
+    }
+
+    public long remainSize(){
+        return size - usedSize;
+    }
+
+    public void updateSize(){
+        usedSize += size;
+    }
+
+    public boolean canStore(long fileSize){
+        return (usedSize + fileSize) <= size;
+    }
 }
