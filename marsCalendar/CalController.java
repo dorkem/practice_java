0340ed799f95537e6642dbf2841d9845fa594db6
dorkem <chlwogur1000@gmail.com>
2025-02-27 19:31:33 +0900

    :sparkles: implete  all method

--- marsCalendar/CalController.java
+++ marsCalendar/CalController.java
@@ -4,6 +4,7 @@ public class CalController {
     UserInput userInput;
     EarthCalendar earthCalendar;
     MarsInfo marsInfo;
+    ProcessBar processBar;
     private long earthDate;
 
     CalController(UserInput userInput) {
@@ -12,10 +13,10 @@ public class CalController {
         this.marsInfo = new MarsInfo();
     }
 
-    public void calculation(){
+    public void calculation() throws InterruptedException {
         String inputData = userInput.inputEarthDate();
         earthDate = earthDate(inputData);
-        if (earthDate != -1) MarsDateCal(earthDate);
+        printResult();
     }
 
     private long earthDate(String string){
@@ -34,6 +35,7 @@ public class CalController {
     }
 
     private void MarsDateCal(long earthDate){
+        earthDate = 738858;
         // 연도 계산
         int twoYearCycle = MarsInfo.YEAR * 2 + 1;
         long marsYear = (earthDate / twoYearCycle) * 2;
@@ -54,6 +56,7 @@ public class CalController {
         for (int i = 1; i <= MarsInfo.MONTH; i++) {
             int monthLength = 28;
 
+            // **비윤년일 때 6개월마다 하루씩 빼지만, 윤년이면 전부 28일 유지**
             if (!isLeapYear && (i % 6 == 0)) {
                 monthLength = 27;
             }
@@ -64,8 +67,45 @@ public class CalController {
             }
             calDate += monthLength;
         }
+
         int marsDay = remainDate - calDate + 1;
 
-        System.out.println(marsYear + " " + marsMonth + " " + marsDay);
+        System.out.println(marsYear + " 화성년 " + marsMonth + "월 " + marsDay + "일");
+
+        printCalendar((int)marsYear, marsMonth, marsDay);
+    }
+
+    private void printResult() throws InterruptedException {
+        this.processBar = new ProcessBar(1000, 20);
+        processBar.loding();
+        System.out.println();
+        System.out.print("지구 날은 "+ earthDate + " => ");
+        if (earthDate != -1) MarsDateCal(earthDate);
+    }
+
+    private void printCalendar(int marsYear, int marsMonth, int marsDay) {
+        System.out.println();
+        System.out.println("     "+ marsYear + "년 " + marsMonth + "월");
+        String arr[][] = new String[5][7];
+        arr[0] = "Su Lu Ma Me Jo Ve Sa".split(" ");
+
+        int date = 1;
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (date > marsDay) {
+                    arr[i][j] = "  ";
+                } else {
+                    arr[i][j] = String.format("%2d", date++);
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(); // 줄바꿈
+        }
+
     }
 }
--- marsCalendar/EarthDefenseHeadquarters.java
+++ marsCalendar/EarthDefenseHeadquarters.java
@@ -1,7 +1,7 @@
 package java_practice.marsCalendar;
 
 public class EarthDefenseHeadquarters {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         UserInput userInput = new UserInput();
         CalController calController = new CalController(userInput);
         calController.calculation();
