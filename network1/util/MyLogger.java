b38a51782efcdfb87f94f2cb65e5a8797c704f83
dorkem <chlwogur1000@gmail.com>
2025-04-08 00:05:39 +0900

    :sparkles: 클라이언트와 서버를 소켓을 통해 연결

---
+++ network1/util/MyLogger.java
@@ -0,0 +1,17 @@
+package java_practice.network1.util;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+public abstract class MyLogger {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    public static void log(Object obj) {
+        String time = LocalTime.now().format(formatter);
+        System.out.printf("%s [%9s] %s\n",
+                time,
+                Thread.currentThread().getName(),
+                obj
+        );
+    }
+}
