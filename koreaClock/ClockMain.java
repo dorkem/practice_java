3190e14af09731660354a715dd82f6d4bedf0317
dorkem <chlwogur1000@gmail.com>
2025-04-08 10:54:08 +0900

    :bulb: 주석 추가

--- koreaClock/ClockMain.java
+++ koreaClock/ClockMain.java
@@ -1,16 +1,16 @@
-package java_practice.koreaClock;
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class ClockMain extends Application {
-    @Override
-    public void start(Stage stage) {
-        ClockUI clockUI = new ClockUI();
-        clockUI.run(stage);
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+//package java_practice.koreaClock;
+//
+//import javafx.application.Application;
+//import javafx.stage.Stage;
+//
+//public class ClockMain extends Application {
+//    @Override
+//    public void start(Stage stage) {
+//        ClockUI clockUI = new ClockUI();
+//        clockUI.run(stage);
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
